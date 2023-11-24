018bd20bc84db114ede365d2d90a097c8047f6fe
flannelmonke <khalilanaji@gmail.com>
2023-11-24 13:37:09 -0500

    change price to type numeric

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE product_catalog(
 		product_code INT PRIMARY KEY NOT NULL,
 		product_brand VARCHAR(55) NOT NULL,
 		quantity INT NOT NULL,
-		unit_price FLOAT NOT NULL
+		unit_price NUMERIC NOT NULL
 	);
