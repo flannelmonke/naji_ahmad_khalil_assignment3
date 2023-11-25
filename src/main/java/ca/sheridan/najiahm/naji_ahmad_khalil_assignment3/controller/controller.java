0ac69417f055f6fbdda3afd9eb7f5227a70a93ac
flannelmonke <khalilanaji@gmail.com>
2023-11-24 19:30:33 -0500

    Added footer to templates/added editing

--- src/main/java/ca/sheridan/najiahm/naji_ahmad_khalil_assignment3/controller/controller.java
+++ src/main/java/ca/sheridan/najiahm/naji_ahmad_khalil_assignment3/controller/controller.java
@@ -14,121 +14,131 @@ import ca.sheridan.najiahm.naji_ahmad_khalil_assignment3.dao.DatabaseAccess;
 import ca.sheridan.najiahm.naji_ahmad_khalil_assignment3.model.Product;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-
 @Controller
 public class controller {
     @Autowired
     private DatabaseAccess pda;
     public ArrayList<Product> prods = new ArrayList<>();
-    @GetMapping(value="/index")//route to index
+
+    @GetMapping(value = "/index") // route to index
     public String index() {
         return "/index";
     }
-    
-    //ADDING ROUTERS
-    @GetMapping("/adding_templates/insert_product")//add to model for form binding, then route user to insert_product_input.html
-    public String insert_prod(Model model){
+
+    // ADDING ROUTERS
+    // add to model for form binding, then route user to
+    // insert_product_input.html
+    @GetMapping("/adding_templates/insert_product")
+    public String insert_prod(Model model) {
         model.addAttribute("product", new Product());
         return "/adding_templates/insert_product_input";
     }
+
     @GetMapping("/insert_outcome")
-    public String insert_outcome(@ModelAttribute Product product, Model model){
-        
+    public String insert_outcome(@ModelAttribute Product product, Model model) {
+
         String message;
         long number_of_rows = pda.addProducts(product);
-        if(number_of_rows > 0){
+        if (number_of_rows > 0) {
             message = "AWESOME SAUCE!";
-        }else{
+        } else {
             message = "OH MY, OH MY, OH MY";
         }
         model.addAttribute("message", message);
         return "/adding_templates/insert_outcome";
     }
 
-
-    //DELETING ROUTING
+    // DELETING ROUTING
     @GetMapping("/delete_product_info")
-    public String delete_selection(Model model){       
-        prods = (ArrayList)pda.selectProducts(0, "");
-        model.addAttribute("products", prods); //showing all objects in database that are deletable
+    public String delete_selection(Model model) {
+        prods = (ArrayList) pda.selectProducts(0, "");
+        model.addAttribute("products", prods); // showing all objects in database that are deletable
         return "/listing_templates/list_all_products";
     }
 
-    //MAPPING FOR SELECTED OBJECT
+    // MAPPING FOR SELECTED OBJECT
     @GetMapping("/delete_product_info/{id}")
     @ResponseBody
-    public String delete_confirm(Model model, @PathVariable("id") int id){
-        String message = "Are you sure you want to delete product with ID: " + id; //requesting confirmation from user
-        String button = "<br/><a href='/delete_product_confirmation/"+id+"'><button>Confirm</button></a>";
+    public String delete_confirm(Model model, @PathVariable("id") int id) {
+        String message = "Are you sure you want to delete product with ID: " + id; // requesting confirmation from user
+        String button = "<br/><a href='/delete_product_confirmation/" + id + "'><button>Confirm</button></a>";
         model.addAttribute("id", id);
-        return message+button;
+        return message + button;
     }
-    //MAPPING FOR WHEN CONFIRM BUTTON IS PRESSED
+
+    // MAPPING FOR WHEN CONFIRM BUTTON IS PRESSED
     @GetMapping("/delete_product_confirmation/{id}")
-    public String delete_outcome(@PathVariable("id") int id, Model model){
+    public String delete_outcome(@PathVariable("id") int id, Model model) {
         long outcome = pda.deleteProduct(id);
         String message;
-        if(outcome>0){
+        if (outcome > 0) {
             message = "AWESOME SAUCE!";
-        }else{
+        } else {
             message = "YOU'RE A DISAPPOINTMENT!";
         }
         model.addAttribute("message", message);
         return "/listing_templates/deleting_templates/delete_outcome";
     }
 
-
-    //EDITING PAGES
-    @GetMapping("list_of_products")
-    public String update_select(Model model){
-        ArrayList<Product> prods = new ArrayList<>();        
-        //TODO: add code to SELECT ALL FROM DATABASE that are editable
-
-        model.addAttribute("Prodcuts", prods); //showing all objects in database that are deletable        
-        return "list_of_courses";
-    }
+    // EDITING PAGES
+    /*
+     * I used our handy array list here since we know the instance the user clicked
+     * on is apart of the list we used to diplay on the page
+     * therefore avoiding having to make 3 requests to the Database to update one
+     * instance, we only need to make two requests.
+     * One of those requests in fact serves the purpose of providing access to both
+     * deleting and editing.
+     */
     @GetMapping("/update_product_info/{id}")
-    public String update_info_form(Model model, @PathVariable("id") int id){
-        Product prod = new Product(); //TODO: CHANGE THIS TO GET STATEMENT RETURN VALUE(Product)
-        model.addAttribute("prod", prod);//added for form binding
-        return "update_product_info";
-    }
-    @GetMapping("update_outcome")
-    public String update_outcome(){
-        //TODO: send update query to SQL with new prod 
-        return "update_outcome";
+    public String update_info_form(Model model, @PathVariable("id") int id) {
+        Product prod = prods.get(id);
+        model.addAttribute("prod", prod);// added for form binding
+        return "/listing_templates/editing_templates/update_product_info";
     }
 
+    @GetMapping("/update_outcome/{id}")
+    public String update_outcome(Model model, @ModelAttribute Product prod, @PathVariable("id") int id) {
+        long result = pda.update_row(id, prod);// given new information provided we use the id to send a request to
+                                               // database to update instance
+        String message;
+        if (result > 0) {
+            message = "AWESOME SAUCE";
+        } else {
+            message = "WAH WAH";
+        }
+        model.addAttribute("message", message);
+        return "/listing_templates/editing_templates/update_outcome";
+    }
 
+    // LISTING PAGES OH MY GOODNESS IT DOESN'T STOP SEND HELP!!!
 
-    //LISTING PAGES OH MY GOODNESS IT DOESN'T STOP SEND HELP!!!
-    
-    //LISTING BRANDS
+    // LISTING BRANDS
     @GetMapping("select_brand")
-    public String brand_picker(Model model){
-        ArrayList<Product> brands = new ArrayList<>();        
-        //TODO: ADD SELECT FUNCTION THAT ADDS ALL UNIQUE BRANDS TO PAGE
+    public String brand_picker(Model model) {
+        ArrayList<Product> brands = new ArrayList<>();
+        // TODO: ADD SELECT FUNCTION THAT ADDS ALL UNIQUE BRANDS TO PAGE
         model.addAttribute("brands", brands);
         return "select_brand";
     }
-    @GetMapping(value="/list_by_brand")
+
+    @GetMapping(value = "/list_by_brand")
     public String banding101() {
-         
+
         return "list_by_brand";
     }
-    
-    //LIST QUANTITIES
+
+    // LIST QUANTITIES
     @GetMapping("select_by_quantity")
-    public String display_then_filter(Model model){
-        ArrayList<Product> prods = new ArrayList<>();        
-        //TODO: ADD SELECT QUERY TO RETURN ALL PRODUCTS UNTIL FILTER IS APPLIED
+    public String display_then_filter(Model model) {
+        ArrayList<Product> prods = new ArrayList<>();
+        // TODO: ADD SELECT QUERY TO RETURN ALL PRODUCTS UNTIL FILTER IS APPLIED
         model.addAttribute("prods", prods);
         return "select_by_quantity";
     }
+
     @GetMapping("/select_by_quantity/{min}")
-    public String display_filtered(@RequestParam ArrayList<Product> prods){
-        //TODO: IMPLEMENT LOGIC FOR FILTERING BY MINIMUM, MAYBE JUST IMPLEMENT IN JS?
+    public String display_filtered(@RequestParam ArrayList<Product> prods) {
+        // TODO: IMPLEMENT LOGIC FOR FILTERING BY MINIMUM, MAYBE JUST IMPLEMENT IN JS?
         return "";
     }
 }
--- src/main/java/ca/sheridan/najiahm/naji_ahmad_khalil_assignment3/dao/DatabaseAccess.java
+++ src/main/java/ca/sheridan/najiahm/naji_ahmad_khalil_assignment3/dao/DatabaseAccess.java
@@ -54,5 +54,19 @@ public class DatabaseAccess {
 
         return rows_affected;
     }
-    
+
+    //Select row method
+    public long update_row(int id, Product prod){
+        MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+        
+        String query = "UPDATE product_catalog SET id = :id, product_code = :code , brand = :brand , quantity = :quantity , unit_price = :price WHERE id = :id";
+        namedParameters.addValue("id", id);
+        namedParameters.addValue("code", prod.getProduct_code());
+        namedParameters.addValue("brand", prod.getBrand());
+        namedParameters.addValue("quantity", prod.getQuantity());
+        namedParameters.addValue("price", prod.getUnit_price());        
+        long rows_affected = jdbc.update(query, namedParameters);
+
+        return rows_affected;
+    }
 }
